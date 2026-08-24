f8bbf28b763be4e08734b92ec41262f03234b868
Victor <lai.victor.lai@gmail.com>
2026-08-24 11:04:33 +0800

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ subprojects {
             minSdk = 23
             targetSdk = 35
 
-            versionName = "2.11.33"
-            versionCode = 211033
+            versionName = "2.11.3388"
+            versionCode = 21103388
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", versionCode.toString())
